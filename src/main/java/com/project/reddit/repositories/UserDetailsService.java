62ffd432ced0da9dbbf9e47ac45badcd362f3e9a
Thinh <thinhmvo101@gmail.com>
2024-04-18 19:32:17 -0400

    changes user detail service

--- src/main/java/com/project/reddit/repositories/UserDetailsService.java
+++ src/main/java/com/project/reddit/repositories/UserDetailsService.java
@@ -3,7 +3,9 @@ package com.project.reddit.repositories;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public interface UseableUserDetail {
+public interface UserDetailsService {
     
     UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException; 
+
+
 }
--- src/main/java/com/project/reddit/services/UserService.java
+++ src/main/java/com/project/reddit/services/UserService.java
@@ -15,8 +15,7 @@ import java.util.Optional;
 @Service
 public class UserService {
 
-    @Autowired
-    UserDetailService userDetailService;
+   
 
     @Autowired
     UserRepo userRepo;
